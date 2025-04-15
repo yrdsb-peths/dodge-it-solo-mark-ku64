c4b9fade25f511f0d0542133acd802a85fb88064
mark-ku64 <mark64ku@gmail.com>
2025-04-15 14:28:34 -0400

    reset banana

--- Bread.java
+++ Bread.java
@@ -5,6 +5,20 @@ public class Bread extends Actor
 {
     public void act()
     {
-        move(-3);
+        move(-10);
+        
+        if (getX() <=0) {
+            resetBread();
+        }
+    }
+    
+    public void resetBread() {
+        int num = Greenfoot.getRandomNumber(2);
+        if (num ==0) {
+            setLocation (600,100);
+        }
+        else {
+        setLocation (600,300);
+    }
     }
 }
